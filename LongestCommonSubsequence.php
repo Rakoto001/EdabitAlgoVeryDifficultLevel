8f403d518e200587c762697c178d852b308fd717
unknown <danielorak1@gmail.com>
2023-08-02 17:43:53 +0200

    all

---
+++ LongestCommonSubsequence.php
@@ -0,0 +1,21 @@
+<?php
+/** link : https://www.hackerrank.com/challenges/dynamic-programming-classics-the-longest-common-subsequence/problem?isFullScreen=true */
+
+class LongestCommonSubsequence
+{
+
+    public function check()  {
+        /**
+         * 5 6
+* 1 2 3 4 1
+* 3 4 1 2 1 3
+         */
+
+         // construct sous chaines du params le plus court => allSubSequences()
+        //  find allSubSequences => from le non plus court 
+
+        
+    }
+    
+  
+}
