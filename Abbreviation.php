4df94b3f3f070148153b1a8b08b5fd20c9ed25aa
Dan <danielorak1@gmail.com>
2023-08-10 20:54:14 +0000

    search best way to make abbreviation aglo

--- Abbreviation.php
+++ Abbreviation.php
@@ -3,31 +3,127 @@
 class Abbreviation
 {
     private array $aSameWords;
-    private int $tmp_index_beta; 
+    private array $tmp_index_beta; 
 
     public function __construct() {
-        $this->tmp_index_alpha = 0;
+        $this->tmp_index_alpha = [];
         $this->aSameWords = [];
     }
 
     public function completeAbbreviationSequence(string $aplha, string $beta)
     {
         $alphaIndex = null;
+        $normalizedValues = [];
 
-        $aAlpha = str_split($aplha);
-        $aBeta = str_split($beta);
+        $normalizedValues = $this->normalizeValue($aplha, $beta);
+        $alphas = $normalizedValues['alpha'];
+        $aBetas = $normalizedValues['beta'];
+       
+        foreach ($aBetas as $keyIndexBeta => $currentBetaStr) {
 
-        foreach ($aBeta as $keyIndexBeta => $currentBetaStr) {
+            if ( !($this->isContent($currentBetaStr, $alphas)) ) {
+                
+                $this->tmp_index_alpha = [];
 
-            $this->tmp_index_alpha = $this->checkUpperAndLOwerAlphaContent($currentBetaStr, $aAlpha);
-            
-            if (count( $this->tmp_index_alpha) > 0) {
-
-                $this->canMakeAbbreviation($aAlpha, $keyIndexBeta);
+            break;
             }
 
         }
+
+        if (!empty($this->tmp_index_alpha)) {
+            $allIndexAlpha = $this->tmp_index_alpha;
+
+            if ($this->operationFesabiity($allIndexAlpha)) {
+
+                // pour opération de supression ou d'éventuel action sur l'ensemble des mots
+                $this->changeSequences();
+            }
+
+        }
+
     }
+
+    public function changeSequences(Type $var = null)
+    {
+        # code...
+    }
+
+
+    /**
+     * 
+     *
+     * @param array $allIndexAlpha
+     * @return boolean
+     */
+    public function operationFesabiity(array $allIndexAlpha) :bool
+    {
+       asort($allIndexAlpha);
+       $sOrderIndexAlpha = implode('', $allIndexAlpha);
+       $stmpIndexAlpha = implode('', $this->tmp_index_alpha);
+
+       if ($stmpIndexAlpha == $sOrderIndexAlpha) {
+
+        return true;
+       }
+        
+        return false;
+    }
+
+     /**
+     * Undocumented function
+     *
+     * @param string $currentBetaStr
+     * @param array $aAlpha
+     * @return boolean
+     */
+    public function isContent(string $currentBetaStr, array $aAlpha) :bool
+    {
+        $indexAlpha = array_search(($currentBetaStr), $aAlpha);
+
+        if (is_numeric($indexAlpha)) {
+
+            $this->tmp_index_alpha[] = $indexAlpha;
+            return true;
+        }
+        
+        return false;
+    }
+
+
+    /**
+     * normalisation de inputs
+     *
+     * @param string $aplha
+     * @param string $beta
+     * @return array
+     */
+    public function normalizeValue(string $aplha, string $beta) :array
+    {
+        $normalizedValues = [];
+        $aAlpha = str_split($aplha);
+        $aBeta = str_split($beta);
+
+        $alphas = array_map(function($a) {
+
+            return strtolower($a);
+
+        }, $aAlpha);
+
+        $aBetas = array_map(function($b) {
+
+            return strtolower($b);
+
+        }, $aBeta);
+
+
+        return [
+            'alpha' => $alphas,
+            'beta' => $aBetas
+        ];
+
+    }
+
+   
 
     public function canMakeAbbreviation(array $aAlpha, $keyIndexBeta)
     {
